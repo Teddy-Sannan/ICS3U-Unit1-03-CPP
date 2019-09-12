7afd2225b0b10780e77ebb0eeaed141942295432
Ubuntu <ubuntu@ip-172-31-16-204.ec2.internal>
2019-09-12 02:19:19 +0000

    C++ Hello World Program but with style

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Teddy Sannan
+// Created on: September 11
+// This is the "Hello, World!" program, but with nice style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
